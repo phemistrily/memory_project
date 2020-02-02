7f467fbe5829c3107ba9297905a4238e6c2eba52
Patryk Świerkowski <phemistrily@gmail.com>
2020-02-02 19:03:27 +0100

    rebuild client to push and receive data

--- src/sample/controllers/MyTimerTask.java
+++ src/sample/controllers/MyTimerTask.java
@@ -3,14 +3,21 @@ package sample.controllers;
 import java.util.TimerTask;
 
 class MyTimerTask extends TimerTask {
+    private final ClientController client;
     GameController param;
 
-    public MyTimerTask(GameController param) {
+    public MyTimerTask(GameController param, ClientController client) {
         this.param = param;
+        this.client = client;
     }
 
     @Override
     public void run() {
         this.param.waitingForPlayer.setVisible(false);
+        this.param.tilesMap = this.client.tilesMap.toArray(new String[0]);
+        for (int i = 0; i < this.param.tilesMap.length;i++)
+        {
+            System.out.println(this.param.tilesMap[i]);
+        }
     }
 }
