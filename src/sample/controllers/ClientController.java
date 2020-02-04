421107f0b595ad7dab765ff5e4d9fa6116e59b7a
Patryk Świerkowski <phemistrily@gmail.com>
2020-02-04 22:42:37 +0100

    update

--- src/sample/controllers/ClientController.java
+++ src/sample/controllers/ClientController.java
@@ -4,13 +4,14 @@ import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import com.google.gson.Gson;
 
 public class ClientController extends Thread {
 
-    protected String playerId;
-    protected Boolean stepLock = false;
     protected ArrayList<String> tilesMap = new ArrayList<String>();
-    PrintWriter writer;
+    private final static Gson GSON = new Gson();
+    public Boolean stepLock = false;
+
     public ClientController() {
     }
 
@@ -19,50 +20,49 @@ public class ClientController extends Thread {
         try {
             final int serverPort = 7171;
             Socket socket = new Socket("localhost", serverPort);
-            OutputStream output = socket.getOutputStream();
-            writer = new PrintWriter(output, true);
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            PrintWriter writer = new PrintWriter(output, true);
             writer.println("Hello to the server, from " + socket.getLocalPort());
             writer.println();
             writer.flush();
 
             InputStream input = socket.getInputStream();
-
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-                switch(line) {
-                    case "PlayerId":
-                        line = reader.readLine();
-                        if(this.playerId == null) {
-                            this.playerId = line;
-                        }
-                        System.out.println(this.playerId);
-                        break;
-                    case "gameStart":
-                        this.stepLock = Boolean.valueOf(reader.readLine());
-                        System.out.println(this.stepLock);
-                        while((line = reader.readLine()) != "endTilesMap")
-                        {
-                            tilesMap.add(line);
-                        }
-                        System.out.println("tilesMap");
-                        System.out.println(this.tilesMap);
-                        break;
+                if (line.equals("get_move")) {
+                    this.stepLock = true;
+                    writer.println(GSON.toJson(
+                            new GameMoveDto(new GameMoveDto.TileDto(1, "img"), new GameMoveDto.TileDto(2, "img"))));
+                    writer.flush();
                 }
+                System.out.println(line);
             }
         }
         catch (IOException ex) {
             System.out.println("Exception " + ex.getMessage());
         }
     }
+    public
 
-    public void makeWrongMove(String[] showImgArr) {
-        writer.println("wrongMove");
-        writer.println(showImgArr[0]);
-        writer.println(showImgArr[1]);
-        writer.println(showImgArr[2]);
-        writer.println(showImgArr[3]);
+    static class GameMoveDto {
+        TileDto tile1;
+        TileDto tile2;
+
+        public GameMoveDto(TileDto tile1, TileDto tile2) {
+            this.tile1 = tile1;
+            this.tile2 = tile2;
+        }
+
+        static class TileDto {
+            int idx;
+            String image;
+
+            public TileDto(int idx, String image) {
+                this.idx = idx;
+                this.image = image;
+            }
+        }
     }
 }
