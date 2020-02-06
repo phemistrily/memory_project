a8567f21d6dd27d8dcf60a44b3631396933cfed2
Patryk Świerkowski <phemistrily@gmail.com>
2020-02-06 03:55:59 +0100

    runable

--- src/sample/controllers/UpdateYourMove.java
+++ src/sample/controllers/UpdateYourMove.java
@@ -1,13 +1,19 @@
 package sample.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class UpdateYourMove implements Runnable {
     private final String ruch;
+    private final GameController param;
 
-    public UpdateYourMove(String ruch) {
+    public UpdateYourMove(GameController param, String ruch) {
+        this.param = param;
         this.ruch = ruch;
     }
 
+    @Override
     public void run() {
-        movePlayerLabel.setText(ruch);
+        param.movePlayerLabel.setText(ruch);
     }
 }
