7df9d498606d1a4771c8ac5cad124490ee1b4548
Patryk Świerkowski <phemistrily@gmail.com>
2020-02-07 21:59:17 +0100

    tableView

---
+++ src/sample/entities/PlayerScoresEntity.java
@@ -0,0 +1,25 @@
+package sample.entities;
+
+import sample.controllers.SqlConnector;
+import sample.controllers.SqlInterface;
+
+import java.sql.ResultSet;
+
+public class PlayerScoresEntity implements SqlInterface {
+    SqlConnector sqlConnector;
+
+    public PlayerScoresEntity() {this.sqlConnector = getLocalhostConnection();}
+    @Override
+    public SqlConnector getLocalhostConnection() {
+        SqlConnector sqlConnector;
+        sqlConnector = new SqlConnector();
+        sqlConnector.getConnection("localhost");
+        return sqlConnector;
+    }
+
+    public ResultSet getScores() {
+        String query = "SELECT uuid as TablePlayerName,score as TablePoints,time as TableTime from player_results";
+        ResultSet scores = sqlConnector.getData(query);
+        return scores;
+    }
+}
